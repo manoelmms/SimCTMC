c6cb31269e2e60a4e26370d6f9c433d33eca0c34
manoelmms <manoelmarcelomms@gmail.com>
2024-07-16 23:54:37 -0300

    Major fixes in simulation

--- sim.py
+++ sim.py
@@ -49,23 +49,24 @@ def simulation(arrival_rate, service_rate, simulation_time, number_of_queues):
         queue_index = random.randint(0, number_of_queues-1)
         # print(f"\nQueue Index: {queue_index} -> Arrival Time: {arrival_time}")
 
-        # Create a job with exponential service time
-        job_time = exponential_random_variable(service_rate)
+        if service_rate >= 0:
+            job_time = exponential_random_variable(service_rate) # Create a job with exponential service time
+        else:
+            job_time = 1 # Create a job with fixed service time (1 second)
         
         # Update the queue free time
-
         if queue_free_time[queue_index] < arrival_time:
             queue_free_time[queue_index] = arrival_time + job_time
+            if queue_free_time[queue_index] <= simulation_time:
+                waiting_time += 0
+                time_in_system += job_time
+                done += 1
         else:
             queue_free_time[queue_index] += job_time
-
-        if queue_free_time[queue_index] <= simulation_time:
-            waiting_time += queue_free_time[queue_index] - arrival_time
-            time_in_system += queue_free_time[queue_index] - arrival_time + job_time
-            done += 1
-        else:
-            # print(f" Job arrived at {arrival_time} could not be processed in queue {queue_index}.") # Higher arrival_time than simulation_time
-            pass
+            if queue_free_time[queue_index] <= simulation_time:
+                waiting_time += queue_free_time[queue_index] - arrival_time - job_time
+                time_in_system += queue_free_time[queue_index] - arrival_time
+                done += 1
 
         if len(arrival_times) == 0: # Arrival list is empty
             break # Stop the simulation when all jobs have been processed
@@ -74,15 +75,18 @@ def simulation(arrival_rate, service_rate, simulation_time, number_of_queues):
     print('\n' + '='*50) 
     # print(queue_free_time)
     print(f"-> Lambda = {arrival_rate}")
-    print(f"-> Mu = {service_rate}")
+
+    if service_rate >= 0:
+        print(f"-> Mu = {service_rate}")
+    else:
+        print(f"-> Mu = 1 (Fixed Service Time)")
+    
     print(f"Number of jobs arrived: {jobs}")
     print(f"Number of jobs completed: {done}")
     print(f"Mean waiting time: {waiting_time/done}")
     print(f"Mean time in the system: {time_in_system/done}")
     print(f"Elapsed Time: {time.time() - elapsed_time}")
     print('='*50)
-
-    print("Simulation finished!")
 
     return time_in_system/done, waiting_time/done
 
@@ -93,18 +97,23 @@ if __name__ == "__main__":
     number_of_queues = 100 # Number of M/M/1 FIFO queues
 
     result = []
-
     for arrival_rate in arrival_rate_list:
         result.append(simulation(arrival_rate, service_rate, simulation_time, number_of_queues))
 
-    plt.plot(arrival_rate_list, [r[0] for r in result], label='Mean Time in System')
-    plt.title('Mean Time in System')
-    plt.xlabel('Arrival Rate')
-    plt.ylabel('Time')
-    plt.show()
+    # Second exercise with fixed service rate
+    service_rate = -1 # mu = 1s (Fixed)
+    result_1 = []
+    for arrival_rate in arrival_rate_list:
+        result_1.append(simulation(arrival_rate, service_rate, simulation_time, number_of_queues))
 
-    # plt.plot(arrival_rate_list, [r[1] for r in result], label='Mean Waiting Time')
-    # plt.title('Mean Waiting Time')
-    # plt.xlabel('Arrival Rate')
-    # plt.ylabel('Time')
-    # plt.show()
+    print("Simulation Finished!")
+
+    # Plotting
+    plt.plot(arrival_rate_list, [r[0] for r in result], label='Mean Time in System', color = 'blue', marker='o')
+    plt.plot(arrival_rate_list, [r[0] for r in result_1], label='Mean Time in System (Fixed Service Time)', color = 'green', marker='o')
+    plt.title('Mean Time in System')
+    plt.xlabel('Arrival Rate(Lambda)')
+    plt.ylabel('Time(s)')
+    plt.legend()
+    plt.grid()
+    plt.show()
--- sim_mp.py
+++ sim_mp.py
@@ -51,23 +51,24 @@ def simulation(arrival_rate, service_rate, simulation_time, number_of_queues):
         queue_index = random.randint(0, number_of_queues-1)
         # print(f"\nQueue Index: {queue_index} -> Arrival Time: {arrival_time}")
 
-        # Create a job with exponential service time
-        job_time = exponential_random_variable(service_rate)
+        if service_rate >= 0:
+            job_time = exponential_random_variable(service_rate) # Create a job with exponential service time
+        else:
+            job_time = 1 # Create a job with fixed service time (1 second)
         
         # Update the queue free time
-
         if queue_free_time[queue_index] < arrival_time:
             queue_free_time[queue_index] = arrival_time + job_time
+            if queue_free_time[queue_index] <= simulation_time:
+                waiting_time += 0
+                time_in_system += job_time
+                done += 1
         else:
             queue_free_time[queue_index] += job_time
-
-        if queue_free_time[queue_index] <= simulation_time:
-            waiting_time += queue_free_time[queue_index] - arrival_time
-            time_in_system += queue_free_time[queue_index] - arrival_time + job_time
-            done += 1
-        else:
-            # print(f" Job arrived at {arrival_time} could not be processed in queue {queue_index}.") # Higher arrival_time than simulation_time
-            pass
+            if queue_free_time[queue_index] <= simulation_time:
+                waiting_time += queue_free_time[queue_index] - arrival_time - job_time
+                time_in_system += queue_free_time[queue_index] - arrival_time
+                done += 1
 
         if len(arrival_times) == 0: # Arrival list is empty
             break # Stop the simulation when all jobs have been processed
@@ -76,7 +77,12 @@ def simulation(arrival_rate, service_rate, simulation_time, number_of_queues):
     print('\n' + '='*50) 
     # print(queue_free_time)
     print(f"-> Lambda = {arrival_rate}")
-    print(f"-> Mu = {service_rate}")
+
+    if service_rate >= 0:
+        print(f"-> Mu = {service_rate}")
+    else:
+        print(f"-> Mu = 1 (Fixed Service Time)")
+
     print(f"Number of jobs arrived: {jobs}")
     print(f"Number of jobs completed: {done}")
     print(f"Mean waiting time: {waiting_time/done}")
@@ -84,31 +90,33 @@ def simulation(arrival_rate, service_rate, simulation_time, number_of_queues):
     print(f"Elapsed Time: {time.time() - elapsed_time}")
     print('='*50)
 
-    print("Simulation finished!")
-
     return time_in_system/done, waiting_time/done
 
 if __name__ == "__main__":
     arrival_rate_list = [0.5, 0.8, 0.9, 0.99] # lambda
-    service_rate = 1 # mu
+    service_rate = [1, -1] # mu [-1 for fixed service Time]
     simulation_time = 10000 # 10000 seconds
     number_of_queues = 100 # Number of M/M/1 FIFO queues
 
-    result = []
+    results = []
+    tuple =  []
 
-    # Create a pool of workers
+    for arrival_rate in arrival_rate_list:
+        for mu in service_rate:
+            tuple.append((arrival_rate, mu, simulation_time, number_of_queues))
+
+    # Multiprocessing
     pool = mp.Pool(mp.cpu_count())
-    result = pool.starmap(simulation, [(arrival_rate, service_rate, simulation_time, number_of_queues) for arrival_rate in arrival_rate_list])
+    results = pool.starmap(simulation, tuple)
     pool.close()
-
-    plt.plot(arrival_rate_list, [r[0] for r in result], label='Mean Time in System')
+    print("Simulation Finished!")
+    
+    # Plotting
+    plt.plot(arrival_rate_list, [results[0][0], results[2][0], results[4][0], results[6][0]], label='Mu = 1', color = 'blue', marker='o')
+    plt.plot(arrival_rate_list, [results[1][0], results[3][0], results[5][0], results[7][0]], label='Mu = 1 (Fixed Service Time)', color = 'green', marker='o')
     plt.title('Mean Time in System')
-    plt.xlabel('Arrival Rate')
-    plt.ylabel('Time')
+    plt.xlabel('Arrival Rate(Lambda)')
+    plt.ylabel('Time(s)')
+    plt.legend()
+    plt.grid()
     plt.show()
-
-    # plt.plot(arrival_rate_list, [r[1] for r in result], label='Mean Waiting Time')
-    # plt.title('Mean Waiting Time')
-    # plt.xlabel('Arrival Rate')
-    # plt.ylabel('Time')
-    # plt.show()
